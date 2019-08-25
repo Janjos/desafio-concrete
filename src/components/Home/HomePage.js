cce742ec3d7905106210fbce1349e7a625be5183
Jonathan <jdamacena.anjos@gmail.com>
2019-08-25 20:09:46 -0300

    home page redirecting to result page

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Logo } from '../common/Logo';
-import { LOGO_SIZES } from '../../constants';
+import { LOGO_SIZES, GITHUB_API_USERS } from '../../constants';
 import searchIcon from '../../assets/icons/search_icon.png';
+import { Link } from 'react-router-dom';
 
 export const HomePage = (props) => {
+    const [userName, setUserName] = useState('');
+
+    const searchUser = () => {
+
+    }
+
     return (
         <div>
             <Logo size={LOGO_SIZES.BIG}></Logo>
@@ -11,10 +18,20 @@ export const HomePage = (props) => {
                 Type the name of the Github user that you want to find
             </label>
             <div>
-                <input type="text" id="search-box"></input>
-                <button>
+                <input
+                    type="text"
+                    id="search-box"
+                    value={userName}
+                    onChange={e => setUserName(e.target.value)}
+                    onKeyPress={e => {
+                        (e.key == 'Enter') && props.history.push(`/user/${userName}`)
+                    }}
+                />
+                <Link
+                    to={`/user/${userName}`}
+                >
                     <img src={searchIcon} alt='Search user'/>
-                </button>
+                </Link>
             </div>
         </div>
     )
