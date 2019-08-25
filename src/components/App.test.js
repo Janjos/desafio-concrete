05abe3757d8b96b2c33a3deae60d057fe5286160
Jonathan <jdamacena.anjos@gmail.com>
2019-08-25 19:39:33 -0300

    Adding moxios to App.test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
+import moxios from 'moxios';
+require('regenerator-runtime/runtime');
 
 import App from "./App";
 import { HomePage } from './Home/HomePage';
 import { ResultPage } from './Result/ResultPage';
 import { appWrapper } from '../testUtils';
-import {act} from 'react-test-renderer';
+import { NotFound } from './NotFound/NotFound';
+import { GITHUB_API_USERS } from '../constants';
+import userMock from '../../__mocks__/userMock';
 
 describe('App', () => {
     it('renders without crash', () => {
@@ -14,15 +18,29 @@ describe('App', () => {
 })
 
 describe('Routes', () => {
+    beforeEach(() => {
+        moxios.install();
+    })
+
+    afterEach(() => {
+        moxios.uninstall();
+    })
+
     it('renders home on "/" path', () => {
         const HomeWrapper = appWrapper('/');
 
         expect(HomeWrapper.find(HomePage)).toHaveLength(1);
     });
 
-    it('renders result page on "/user" path', () => {
-        const ResultWrapper = appWrapper('/user/username');
-        
-        expect(ResultWrapper.find(ResultPage)).toHaveLength(1);
-    })
+    it('renders result page on "/user" path', async () => {
+        const ResultWrapper = await appWrapper('/user/timbl');
+
+        expect(ResultWrapper.find(ResultPage)).toHaveLength(1);        
+    });
+
+    it('renders not found page on "/notfound" path', () => {
+        const NotFoundWrapper = appWrapper('/notfound');
+
+        expect(NotFoundWrapper.find(NotFound)).toHaveLength(1);
+    });
 })
