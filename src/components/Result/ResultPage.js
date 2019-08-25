0cb53fadc4d9980361a18fe8fdc04a196b0f40a9
Jonathan <jdamacena.anjos@gmail.com>
2019-08-25 20:33:40 -0300

    Adding other info to resultPage

--- src/components/Result/ResultPage.js
+++ src/components/Result/ResultPage.js
@@ -20,8 +20,40 @@ export const ResultPage = ({match, history}) => {
         <div>
             <div className='user'>
                 <img className='user_avatar' src={user.avatar_url} alt={`${user.name} avatar`} />
-                <h2 className='user_login'> {user.login} </h2>
-                <h3 className='user_name'>{user.name}</h3>
+                <h2 className='user_name'>{user.name}</h2>
+                <h3 className='user_login'> {user.login} </h3>
+                <div className='user_meta'>
+                    <div>
+                        <img />
+                        <p className='user_meta_company'>
+                            {user.company}
+                        </p>
+                    </div>
+                    <div>
+                        <img />
+                        <p className='user_meta_location'>
+                            {user.location}
+                        </p>
+                    </div>
+                    <div>
+                        <img />
+                        <p className='user_meta_stars'>
+                            13
+                        </p>
+                    </div>
+                    <div>
+                        <img />
+                        <p className='user_meta_repos'>
+                            {user.public_repos}
+                        </p>
+                    </div>
+                    <div>
+                        <img />
+                        <p className='user_meta_followers'>
+                            {user.followers}
+                        </p>
+                    </div>
+                </div>
             </div>
             <div className='repos'>
                 <ul>
